8db96d2121ed281379c437aa538ad177c5f75bc8
Anna Lavrishcheva <annalavrishcheva5@yandex.ru>
2023-01-14 17:44:20 +0300

    Поправила роуты

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
+
+const ERROR_INCORRECT_INFO = 400;
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -12,8 +15,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 const app = express();
 
-app.use('/', usersRouter);
-app.use('/', cardsRouter);
+app.use((req, res, next) => {
+  req.user = {
+    _id: 'какой-то id, который я никак не могу получить',
+  };
+
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+app.use((req, res) => {
+  res.status(ERROR_INCORRECT_INFO).send({ message: 'Ошибка 404: страница не существует' });
+});
 
 app.listen(PORT, () => {
   console.log('Приложение работает');
