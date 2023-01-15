fd6be1753bd6d9d9c36ff897ae7c19905829c748
Anna Lavrishcheva <annalavrishcheva5@yandex.ru>
2023-01-15 11:13:54 +0300

    поправила роуты в карточках

--- routes/cards.js
+++ routes/cards.js
@@ -8,10 +8,10 @@ const {
   deleteLike,
 } = require('../controllers/cards');
 
-cardsRouter.get('/cards', getAllCards);
-cardsRouter.post('/cards', createNewCard);
-cardsRouter.delete('/cards/:cardId', deleteCard);
-cardsRouter.put('/cards/:cardId/likes', setLike);
-cardsRouter.delete('/cards/:cardId/likes', deleteLike);
+cardsRouter.get('/', getAllCards);
+cardsRouter.post('/', createNewCard);
+cardsRouter.delete('/:cardId', deleteCard);
+cardsRouter.put('/:cardId/likes', setLike);
+cardsRouter.delete('/:cardId/likes', deleteLike);
 
 module.exports = cardsRouter;
