2b12f733f22ac8377aac6b0189ff05ef063cd62a
Artem <artem.zvonilkin@mail.ru>
2023-01-27 18:05:41 +0300

    тесты 5

--- app.js
+++ app.js
@@ -17,14 +17,11 @@ mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
-app.use(errors());
-app.use(auth);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
-app.use('*', (_, __, next) => next(new NotFoundError('Такой страници не существует')));
 
+//роуты, которым не нужна авторизация
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -41,6 +38,14 @@ app.post('/signup', celebrate({
     password: Joi.string().required(),
   }),
 }), createNewUser);
+
+/*app.use(auth);*/
+//роуты, которым нужна авторизация
+app.use('/users', auth, usersRouter);
+app.use('/cards', auth, cardsRouter);
+
+app.use(errors());
+app.use('*', (_, __, next) => next(new NotFoundError('Такой страници не существует')));
 
 app.use((error, req, res, next) => {
   if (error.statusCode) {
