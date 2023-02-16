301fd87deb50c66f9525e2cfabab7ef0d9f446a9
Anna Lavrishcheva <annalavrishcheva5@yandex.ru>
2023-02-16 16:33:18 +0300

    и снова cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const cors = require('./middlewares/cors');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-/* const allowedCors = {
+ const allowedCors = {
   origin: [
     'https://morgenmuffel.study.nomoredomains.work',
     'http://morgenmuffel.study.nomoredomains.work',
@@ -30,14 +30,14 @@ const app = express();
   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
   credentials: true,
 };
-app.use('*', cors(allowedCors)); */
+app.use('*', cors(allowedCors));
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use(cors);
+/* app.use(cors); */
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
